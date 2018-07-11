195aef03d4aa2af3724e934e6d70239c97d2771a
zhufo44 <1126760657@qq.com>
2018-07-11 15:10:43 +0800

    修改type溢出

--- scrapy_tencent/TenCent/TenCent/spiders/tenposition.py
+++ scrapy_tencent/TenCent/TenCent/spiders/tenposition.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+import scrapy
 from scrapy import Spider,Field
 from TenCent.items import TencentItem
 
@@ -13,17 +14,17 @@ class TenPosition(Spider):
 
     def parse(self,response):
 
-        for tr in response.xpath('//tr/@class="even" | //tr/@class="odd"'):
+        for tr in response.xpath('//tr[@class="even"] | //tr[@class="odd"]'):
             item = TencentItem()
-            item[posit_name] = tr.xpath('td[1]/a/text()').extract()[0]
-            item[posit_link] = tr.xpath('td[1]/a/@href').extract()[0]
+            item['posit_name'] = tr.xpath('td[1]/a/text()').extract()[0]
+            item['posit_link'] = tr.xpath('td[1]/a/@href').extract()[0]
             if tr.xpath('td[2]/text()').extract()[0]:
-                item[posit_type] = tr.xpath('td[2]/text()').extract()[0]
+                item['posit_type'] = tr.xpath('td[2]/text()').extract()[0]
             else:
-                item[posit_type] = None
-            item[people_num] = tr.xpath('td[3]/text()').extract()[0]
-            item[location] = tr.xpath('td[4]/text()').extract()[0]
-            item[pub_time] = tr.xpath('td[5]/text()').extract()[0]
+                item['posit_type'] = None
+            item['people_num'] = tr.xpath('td[3]/text()').extract()[0]
+            item['location'] = tr.xpath('td[4]/text()').extract()[0]
+            item['pub_time'] = tr.xpath('td[5]/text()').extract()[0]
 
             yield item
         if self.offset < 3740:
